5959ca5175490087bcb0c795b6413a979bb3ece0
Vlad Daneliuc <daneliuc.vlad@gmail.com>
2018-03-26 14:13:33 +0300

    added tableView + cell
    added a tableview in storyboards + created class for it and cell

---
+++ meet/meet/Controllers/MyItineraryTableViewController.swift
@@ -0,0 +1,54 @@
+//
+//  MyItineraryTableViewController.swift
+//  meet
+//
+//  Created by Vlad Daneliuc on 26/03/2018.
+//  Copyright © 2018 AgileFreaks. All rights reserved.
+//
+
+import UIKit
+
+class MyItineraryTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+extension MyItineraryTableViewController {
+        // MARK: - Table view data source
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "itinerary", for: indexPath)
+        
+        // Configure the cell...
+        
+        return cell
+    }
+}
---
+++ meet/meet/Views/MyItineraryTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MyItineraryTableViewCell.swift
+//  meet
+//
+//  Created by Vlad Daneliuc on 26/03/2018.
+//  Copyright © 2018 AgileFreaks. All rights reserved.
+//
+
+import UIKit
+
+class MyItineraryTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
